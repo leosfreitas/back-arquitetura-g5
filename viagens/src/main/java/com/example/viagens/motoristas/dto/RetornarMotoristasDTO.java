cb70c5b92c9d95c5e55b7b5d551bb35075b4d6dd
leosfreitas <leonardosf@al.insper.edu.br>
2024-05-02 14:23:23 -0300

    fix

--- viagens/src/main/java/com/example/viagens/motoristas/dto/RetornarMotoristasDTO.java
+++
@@ -1,4 +0,0 @@
-package com.example.viagens.motoristas.dto;
-
-public class RetornarMotoristasDTO {
-}
--- viagens/src/main/java/com/example/viagens/motoristas/model/Motoristas.java
+++ viagens/src/main/java/com/example/viagens/motoristas/model/Motoristas.java
@@ -1,10 +1,8 @@
 package com.example.viagens.motoristas.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Motoristas {
 
     @Id
